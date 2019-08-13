789b99b81d85b41ba3205de0d45ca3a4f2ce3991
TJ Epperson <tj.epperson070@gmail.com>
2019-08-13 10:08:56 +0900

    added config

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+
+module.exports = {
+  client: "pg",
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: true
+  },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    tableName: "knex_migrations",
+    directory: "./migrations",
+  },
+  ssl: true
+};
