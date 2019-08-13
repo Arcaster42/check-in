9bb8e1d0a813346c4cde64a5c352c6b0e5bdda29
TJ Epperson <tj.epperson070@gmail.com>
2019-08-13 15:39:41 +0900

    added authentication

---
+++ src/components/Login.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="page">
+        <form class="form">
+            <label for="username">Username</label><br>
+            <input class="textbox" type="text" v-model="username"><br>
+            <label for="password">Password</label><br>
+            <input class="textbox" type="password" v-model="password"><br>
+            <input type="submit" value="Login" @click="loginClick"><br>
+            <button @click="registerClick">Register</button><br>
+        </form>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data: () => ({
+        username: '',
+        password: ''
+    }),
+    methods: {
+        loginClick(event) {
+            event.preventDefault()
+            const user = {
+                username: this.username,
+                password: this.password
+            }
+            axios.post('http://localhost:3000/api/login', user)
+            .then((result) => console.log(result.data))
+        },
+        registerClick(event) {
+            event.preventDefault()
+            this.$store.commit('setView', 'Register')
+        }
+    }
+}
+</script>
+
+<style scoped>
+.page {
+    display: flex;
+    flex-direction: column;
+    margin-top: 10%;
+    height: 100%;
+    width: 100%;
+}
+</style>
