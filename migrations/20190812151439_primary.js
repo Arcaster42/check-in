b2c1e93f2088fc4c1315d5777eb2a3386687554c
TJ Epperson <tj.epperson070@gmail.com>
2019-08-13 15:39:09 +0900

    added checks table

--- migrations/20190812151439_primary.js
+++ migrations/20190812151439_primary.js
@@ -1,15 +1,30 @@
 
 exports.up = function(knex) {
-  return knex.schema.createTable('accounts', (t) => {
-    t.string('name').unsigned().primary()
-    t.string('email')
-    t.string('first_name')
-    t.string('last_name')
-    t.string('phone')
-    t.text('pass_hash')
-  })
+  const createAccounts = () => {
+    knex.schema.createTable('accounts', (t) => {
+      t.string('username').unsigned().primary()
+      t.string('email')
+      t.string('first_name')
+      t.string('last_name')
+      t.string('phone')
+      t.text('pass_hash')
+    })
+  }
+  
+  const createChecks = () => {
+    knex.schema.createTable('checks', (t) => {
+      t.increment().primary()
+      t.string('username').references('accounts.username')
+      t.timestamp('check_in')
+      t.timestamp('check_limit')
+      t.boolean('alarm')
+    })
+  }
+
+  return createAccounts()
+    .then(() => createChecks())
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTable('accounts')
+  return knex.schema.dropTable('accounts').dropTable('checks')
 };
