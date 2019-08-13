cfb669593d034f854a186a8300b49fbb604d4adf
TJ Epperson <tj.epperson070@gmail.com>
2019-08-13 15:38:27 +0900

    added login and register

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <transition name="fade" mode="out-in">
+      <Splash v-if="getView==='Splash'"/>
+      <Login v-if="getView==='Login'"/>
+      <Register v-if="getView==='Register'"/>
+    </transition>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import Splash from './components/Splash'
+import Login from './components/Login'
+import Register from './components/Register'
 
 export default {
   name: 'app',
   components: {
-
+    Splash,
+    Login,
+    Register
+  },
+  computed: {
+    getView() {
+      return this.$store.state.view
+    }
   },
   mounted() {
+    setTimeout(() => {
+      this.$store.commit('setView', 'Splash')
+    }, 1000)
+    setTimeout(() => {
+      this.$store.commit('setView', 'Login')
+    }, 2500)
     // fetch('http://localhost:3000/api').then((result) => {return result.json()}).then((reply) => console.log(reply))
     // axios.get('http://localhost:3000/api').then((query) => {console.log(query.data)})
     // axios.post('http://localhost:3000/api/account', {username: 'test'}).then((response) => console.log(response))
@@ -27,6 +47,13 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
