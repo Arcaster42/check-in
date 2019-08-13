0962c2a331b1bafe947faa643cc23bae1c6f95d4
TJ Epperson <tj.epperson070@gmail.com>
2019-08-13 15:39:50 +0900

    added registration

---
+++ src/components/Register.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="page">
+        <p>Registration</p>
+        <form class="form">
+            <label for="username">Username</label><br>
+            <input class="textbox" type="text" v-model="username"><br>
+            <label for="email">E-Mail</label><br>
+            <input class="textbox" type="email" v-model="email"><br>
+            <label for="firstname">First Name</label><br>
+            <input class="textbox" type="text" v-model="firstName"><br>
+            <label for="lastname">Last Name</label><br>
+            <input class="textbox" type="text" v-model="lastName"><br>
+            <label for="phone">Phone Number</label><br>
+            <input class="textbox" type="text" v-model="phone"><br>
+            <label for="password">Password</label><br>
+            <input class="textbox" type="password" v-model="password"><br>
+            <label for="password2">Password</label><br>
+            <input class="textbox" type="password" v-model="password2"><br>
+            <input type="submit" value="Register" @click="registerClick"><br>
+            <button @click="cancelClick">Cancel</button><br>
+        </form>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    data: () => ({
+        username: null,
+        email: null,
+        firstName: null,
+        lastName: null,
+        phone: null,
+        password: null,
+        password2: null,
+        code: null
+    }),
+    methods: {
+        registerClick(event) {
+            event.preventDefault()
+            const user = {
+                username: this.username,
+                email: this.email,
+                firstName: this.firstName,
+                lastName: this.lastName,
+                phone: this.phone,
+                password: this.password
+            }
+            axios.post('http://localhost:3000/api/account', user)
+        },
+        cancelClick(event) {
+            event.preventDefault()
+            this.$store.commit('setView', 'Login')
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
