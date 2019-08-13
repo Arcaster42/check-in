a2be6243ebd9b4be6e02783dc29b02cfbbdd8017
TJ Epperson <tj.epperson070@gmail.com>
2019-08-13 15:39:20 +0900

    implemented vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import Vuex from 'vuex'
+Vue.use(Vuex)
 Vue.config.productionTip = false
 
+const store = new Vuex.Store({
+  state: {
+    view: 'None',
+    user: null,
+  },
+  getters: {
+
+  },
+  mutations: {
+    setView(state, view) {
+      state.view = view
+    }
+  },
+  actions: {
+
+  }
+})
+
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
