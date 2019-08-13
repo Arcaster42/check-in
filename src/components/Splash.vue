97f3f02407164b13818cb6202112d4060c35e00b
TJ Epperson <tj.epperson070@gmail.com>
2019-08-13 15:39:55 +0900

    created splash

---
+++ src/components/Splash.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="splash">
+        <span class="splash-title">CHECK-IN</span>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>  
+    .splash {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        width: 100%;
+        height: 100%;
+        min-height: 800px;
+        background-color: lightblue;
+    }
+
+    .splash-title {
+        font-size: 40px;
+        font-weight: bolder;
+    }
+</style>
