5abb5dd0070a9e03c10ae55ade947c556a401ef9
TJ Epperson <tj.epperson070@gmail.com>
2019-08-14 09:31:42 +0900

    adjusted express.static

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000
 const cors = require('cors')
 const path = require('path')
 const bodyParser = require('body-parser')
-app.use(express.static('public'))
+app.use(express.static(__dirname + 'public'))
 app.use(bodyParser.json())
 app.use(cors())
 
